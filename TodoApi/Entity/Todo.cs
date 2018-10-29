cb4b5d9c4b19eefff48d238499495439f3d93d1e
Dinh Nam <dnzakmin@gmail.com>
2018-10-29 12:04:01 +0700

    Oke
    Oke

---
+++ TodoApi/Entity/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Entity
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ TodoApi/Entity/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Entity;
+
+namespace TodoApi.Entity
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoApi.Entity.Todo> Todo { get; set; }
+    }
+}
